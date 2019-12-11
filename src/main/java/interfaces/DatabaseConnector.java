282aeb84bfe8a966ec3fe921d2908b5ebd328333
MikiKru <michal_kruczkowski@o2.pl>
2019-12-11 18:03:56 +0100

    interfaces v1

---
+++ src/main/java/interfaces/DatabaseConnector.java
@@ -0,0 +1,14 @@
+package interfaces;
+// definicja interfajsu
+public interface DatabaseConnector {
+    // pole finalne -> stała
+    public final String PORT = "3306";
+    // pole statyczne -> zmienna statyczna
+    public static final String HOST = "localhost";
+
+    // metoda abstrakcyjna -> bez ciała metody {implementacji}
+    boolean databaseDriverCheck();
+    void setConnection(String username, String password, String databaseName);
+    void closeConnecion();
+
+}
---
+++ src/main/java/interfaces/DatabaseConnectorImpl.java
@@ -0,0 +1,26 @@
+package interfaces;
+// klasa implementująca interfejs
+public class DatabaseConnectorImpl implements DatabaseConnector {
+    private boolean isDriverInstalled = true;
+    @Override
+    public boolean databaseDriverCheck() {
+        return isDriverInstalled;   // sprawdza czy jest zainstalowany driver w projekcie
+    }
+    @Override
+    public void setConnection(String username, String password, String databaseName) {
+        System.out.println("CONNECTED");
+        // Odwołanie do składowej finalnej interfejsu
+        // Odowłanie do składowej statycznej
+        System.out.println(DatabaseConnector.HOST + ":" + DatabaseConnector.PORT + "/" + databaseName);
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+    }
+    @Override
+    public void closeConnecion() {
+        System.out.println("CONNECION IS CLOSED");
+    }
+    public void executeQuerry(String SQL){
+        System.out.println("Query: " + SQL + " is executed");
+    }
+
+}
