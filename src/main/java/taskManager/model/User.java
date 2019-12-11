a0dce48c1db8b53f2fb259be9810a1bae6716860
MikiKru <michal_kruczkowski@o2.pl>
2019-12-11 19:38:29 +0100

    interfaces v7

--- src/main/java/taskManager/model/User.java
+++ src/main/java/taskManager/model/User.java
@@ -1,4 +1,19 @@
 package taskManager.model;
+import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+// adnotacja generująca getery i setery do wszystkich pól klasowych + toString
+@Data
 public class User {
+    private String name;
+    private String lastName;
+    private List<Task> tasks = new LinkedList<>();
+
+    public User(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
 }
