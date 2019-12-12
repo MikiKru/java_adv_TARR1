68707db3f461d8a8fe6108badc87799a26f78e31
MikiKru <michal_kruczkowski@o2.pl>
2019-12-12 19:52:38 +0100

    tasks manager v5

--- src/main/java/fileIO/FileService.java
+++ src/main/java/fileIO/FileService.java
@@ -1,7 +1,6 @@
 package fileIO;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class FileService {
@@ -33,12 +32,32 @@ public class FileService {
             System.out.println("LASTNAME: " +elements[2]);
             System.out.println("==========================");
         }
+        s.close();
+    }
+    public void insertDataIntoFile(String path, String data) throws FileNotFoundException {
+        // nie wymaga aby plik istniał
+        // zostanie automatycznie utworzony nowy plik
+        // wykonuje nadpisanie istniejącej zawartości pliku
+        PrintWriter pw = new PrintWriter(new File(path));
+        pw.println("NAGŁÓWEK");
+        pw.println(data);
+        pw.close();
+    }
+    public void appendDataIntoFile(String path, String data) throws IOException {
+        FileWriter fw = new FileWriter(new File(path), true);
+        fw.append(data+"\n");
+        fw.close();
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         FileService fs = new FileService();
-        fs.readFile("C:\\Users\\PROXIMO\\Desktop\\TARR1\\java_advanced\\src\\main\\java\\fileIO\\text.txt");
-        fs.readCsvFile("C:\\Users\\PROXIMO\\Desktop\\TARR1\\java_advanced\\src\\main\\java\\fileIO\\data.csv");
+//        fs.readFile("C:\\Users\\PROXIMO\\Desktop\\TARR1\\java_advanced\\src\\main\\java\\fileIO\\text.txt");
+//        fs.readCsvFile("C:\\Users\\PROXIMO\\Desktop\\TARR1\\java_advanced\\src\\main\\java\\fileIO\\data.csv");
+//        fs.insertDataIntoFile(
+//                "C:\\Users\\PROXIMO\\Desktop\\TARR1\\java_advanced\\src\\main\\java\\fileIO\\fromFileReader.txt",
+//                "ALA MA PSA");
+        fs.appendDataIntoFile("C:\\Users\\PROXIMO\\Desktop\\TARR1\\java_advanced\\src\\main\\java\\fileIO\\append.txt",
+                "dodajemy dane");
     }
 
 }
---
+++ src/main/java/taskManager/controller/FileManager.java
@@ -0,0 +1,5 @@
+package taskManager.controller;
+
+public interface FileManager {
+    public final String FILE_PATH = "C:\\Users\\PROXIMO\\Desktop\\TARR1\\java_advanced\\src\\main\\java\\taskManager\\resource\\tasks.csv";
+}
