cee432fce3d284c8cc46d989d3ea9372b8c618aa
MikiKru <michal_kruczkowski@o2.pl>
2019-12-16 18:36:56 +0100

    exceptions v2

--- src/main/java/exceptions/UserInteractions.java
+++ src/main/java/exceptions/UserInteractions.java
@@ -1,20 +1,50 @@
 package exceptions;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserInteractions {
-    public void getUserData(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj imię");
-        String name = scanner.nextLine();
-        System.out.println("Podaj datę urodzenia");
-        LocalDate birthDate = LocalDate.parse(scanner.nextLine());  // wprowadzam napis i parsuje do daty
-        System.out.println("Podaj liczbę całkowitą");
-        int integerNumber = scanner.nextInt();
-        System.out.println("Podaj liczbę zmiennprzecinkową");
-        double doubleNumber = scanner.nextDouble();
-        System.out.printf("Twoje dane: %s, %s, %d, %f\n", name, birthDate, integerNumber, doubleNumber);
+    private Scanner scanner = new Scanner(System.in);
+    private String name;
+    private LocalDate birthDate;
+    private int integerNumber;
+    private double doubleNumber;
+
+    public void getUserData() {
+        boolean dateException = true;
+        boolean intException = true;
+        boolean doubleException = true;
+        while (true) {
+            try {
+                if(dateException) {
+                    System.out.println("Podaj imię");
+                    name = scanner.nextLine();
+                    System.out.println("Podaj datę urodzenia");
+                    birthDate = LocalDate.parse(scanner.nextLine());  // wprowadzam napis i parsuje do daty
+                    dateException = false;
+                }
+                if(intException) {
+                    System.out.println("Podaj liczbę całkowitą");
+                    integerNumber = scanner.nextInt();
+                    intException = false;
+                }
+                if(doubleException){
+                    System.out.println("Podaj liczbę zmiennprzecinkową");
+                    doubleNumber = scanner.nextDouble();
+                    System.out.printf("Twoje dane: %s, %s, %d, %f\n", name, birthDate, integerNumber, doubleNumber);
+                    doubleException = false;
+                }
+                break;
+            } catch (DateTimeParseException e) {
+                System.out.println("Podałeś błędną datę");
+            } catch (InputMismatchException e) {
+                System.out.println("Podałeś błędną liczbę");
+            } finally {
+                scanner.nextLine();
+            }
+        }
     }
 
     public static void main(String[] args) {
