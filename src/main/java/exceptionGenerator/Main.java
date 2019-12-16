a513372f3af15c5fbebcf211d3c17d42f4cc209e
MikiKru <michal_kruczkowski@o2.pl>
2019-12-16 20:11:15 +0100

    exceptions v10

--- src/main/java/exceptionGenerator/Main.java
+++ src/main/java/exceptionGenerator/Main.java
@@ -1,5 +1,9 @@
 package exceptionGenerator;
 
+import exceptionGenerator.exceptions.MyException1;
+import exceptionGenerator.exceptions.MyException2;
+import exceptionGenerator.exceptions.MyException3;
+
 import java.util.Random;
 
 public class Main {
@@ -14,16 +18,24 @@ public class Main {
         // printStackTarace() -> zawraca Klasę w której wystąpił dany wyjątek
         Random random = new Random();
         int randomInteger = random.nextInt(3); // losuje wartości 0 lub 1 lub 2
-        switch (randomInteger){
-            case 0:
-                System.out.println(0);
-                break;
-            case 1:
-                System.out.println(1);
-                break;
-            case 2:
-                System.out.println(2);
-                break;
+        try {
+            switch (randomInteger) {
+                case 0:
+                    throw new MyException1();
+                case 1:
+                    throw new MyException2();
+                case 2:
+                    throw new MyException3();
+            }
+        } catch (MyException1 e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } catch (MyException2 e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } catch (MyException3 e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
