e3792770de9345a1b6c2ff2a573dc9dd968f5106
MikiKru <michal_kruczkowski@o2.pl>
2019-12-17 18:52:12 +0100

    christmas tree v1

--- src/main/java/christmasExercises/ChristmasTreeImpl.java
+++ src/main/java/christmasExercises/ChristmasTreeImpl.java
@@ -5,8 +5,6 @@ public class ChristmasTreeImpl implements ChristmasTree {
 
     @Override
     public void drawChristmasTree(int n) {
-
-
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= (2 * n - 1); j++) {
                 if (j <= (n - i)) {
@@ -14,15 +12,27 @@ public class ChristmasTreeImpl implements ChristmasTree {
                 }
             }
             for (int k = 1; k <= (2 * n - 1); k++) {
-                if (k <= (2 * i - 1)) {
+                if (i==1 && k==1) {
+                    System.out.print("^");
+                }
+                else if (k <= (2 * i - 1)) {
                     System.out.print("*");
                 }
             }
             System.out.println();
+            if(i == n) {
+                for (int l = 1; l < (2 * i - 1); l++) {
+                    if (l <= (2*n - 1)/2) {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println("H");
+            }
         }
+
     }
 
     public static void main(String[] args) {
-        new ChristmasTreeImpl().drawChristmasTree(4);
+        new ChristmasTreeImpl().drawChristmasTree(10);
     }
 }
