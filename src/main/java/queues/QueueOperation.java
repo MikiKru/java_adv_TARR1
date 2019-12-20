3e78fce6f835833c0b759ebd645f4847d9aa7eb5
MikiKru <michal_kruczkowski@o2.pl>
2019-12-20 19:56:49 +0100

    queue v1

--- src/main/java/queues/QueueOperation.java
+++ src/main/java/queues/QueueOperation.java
@@ -1,4 +1,31 @@
 package queues;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
 public class QueueOperation {
+    // |mN|...|m3|m2|m1|
+    Queue<String> messages = new ArrayDeque<>();       // kolejka jednokierunkowa
+
+    public void addMessage(String content){
+        messages.add(content);          // dodaje element na koniec kolejki
+    }
+    public void showMessages(){
+        messages.forEach(System.out::println);
+    }
+    public String removeElement(){
+        return messages.remove();          // usuwa element z początku kolejki
+    }
+
+    public static void main(String[] args) {
+        QueueOperation q = new QueueOperation();
+        q.addMessage("M1");
+        q.addMessage("M2");
+        q.addMessage("M3");
+        q.addMessage("M4");
+        q.showMessages();
+        System.out.println("==================");
+        System.out.println(q.removeElement());
+    }
 }
