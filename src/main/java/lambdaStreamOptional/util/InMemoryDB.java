f6e50143946b102a015f7a2bd5b807008063fdb2
MikiKru <michal_kruczkowski@o2.pl>
2019-12-20 17:57:17 +0100

    lambda on user v2

--- src/main/java/lambdaStreamOptional/util/InMemoryDB.java
+++ src/main/java/lambdaStreamOptional/util/InMemoryDB.java
@@ -9,7 +9,14 @@ import java.util.List;
 public interface InMemoryDB {
     List<User> users = new ArrayList<>(
             Arrays.asList(
-                 new User(),
+                 new User("mk@mk.pl", "mk"),
+                 new User("kk@kk.pl", "kk"),
+                 new User("tt@tt.pl", "tt"),
+                 new User("rr@rr.pl", "rr"),
+                 new User("ee@ee.pl", "ee"),
+                 new User("ww@ww.pl", "ww"),
+                 new User("qq@qq.pl", "qq"),
+                 new User("ii@ii.pl", "ii")
             )
     );
 
